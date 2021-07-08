4cccd2806a7a462d724eec7f039995d3737b58d0
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-07-08 21:18:35 +0900

    fix: mockのidの型をstringに修正

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -6,11 +6,11 @@ export const handlers = [
       ctx.status(200),
       ctx.json([
         {
-          id: 1,
+          id: '1',
           name: 'foo',
         },
         {
-          id: 2,
+          id: '2',
           name: 'bar',
         },
       ])
