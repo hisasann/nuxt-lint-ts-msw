d5bc99453d3318b3e96cd7e2daac23dd41d3bb76
hisamatsu yoshiyuki <hisasann25@gmail.com>
2022-02-13 02:51:19 +0900

    chore(package.json): upgrade nuxt2 to nuxt bridge

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
-export default {
+import { defineNuxtConfig } from '@nuxt/bridge';
+
+export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
@@ -39,7 +41,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    // '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -51,4 +53,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
-};
+});
